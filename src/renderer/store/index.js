f1bfd0a10666d78583b62e3f56bed068269ef7ea
Fedde van Gils <feddevangils@gmail.com>
2020-02-04 06:27:06 +0100

    added icons

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import VueElectron from 'vue-electron'
 import { createPersistedState, createSharedMutations } from 'vuex-electron'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faTransgender, faVenus, faMars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add([faUserSecret, faVenus, faMars, faTransgender])
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 import modules from './modules'
 
