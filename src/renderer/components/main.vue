de97eea092190ebb608fc24b0a69328567108cdf
Fedde van Gils <feddevangils@gmail.com>
2020-03-05 10:12:52 +0100

    add nfc library

--- src/renderer/components/main.vue
+++ src/renderer/components/main.vue
@@ -160,14 +160,18 @@
 </template>
 
 <script>
+    
     // drag library for dragging items
   import draggable from 'vuedraggable'
     // initialises menu object
   import {Menu} from 'vue-electron'
-    
+import { NFC } from 'nfc-pcsc';
+
     // for usage of selecting something from the file system.
     const fileDialog = require('file-dialog');
 
+    const nfc = new NFC();
+    
     // for usage of 
     const Dialog = require('dialogs');
     const dialog = Dialog();
@@ -221,6 +225,53 @@
                 this.assignParticipants();
             }
         }, 2500 )
+
+        nfc.on('reader', reader => {
+
+            console.log(`${reader.reader.name}  device attached`);
+
+            // enable when you want to auto-process ISO 14443-4 tags (standard=TAG_ISO_14443_4)
+            // when an ISO 14443-4 is detected, SELECT FILE command with the AID is issued
+            // the response is available as card.data in the card event
+            // you can set reader.aid to:
+            // 1. a HEX string (which will be parsed automatically to Buffer)
+            reader.aid = 'F222222222';
+            // 2. an instance of Buffer containing the AID bytes
+            // reader.aid = Buffer.from('F222222222', 'hex');
+            // 3. a function which must return an instance of a Buffer when invoked with card object (containing standard and atr)
+            //    the function may generate AIDs dynamically based on the detected card
+            // reader.aid = ({ standard, atr }) => {
+            //
+            // 	return Buffer.from('F222222222', 'hex');
+            //
+            // };
+
+            reader.on('card', card => {
+
+                // card is object containing following data
+                // [always] String type: TAG_ISO_14443_3 (standard nfc tags like MIFARE) or TAG_ISO_14443_4 (Android HCE and others)
+                // [always] String standard: same as type
+                // [only TAG_ISO_14443_3] String uid: tag uid
+                // [only TAG_ISO_14443_4] Buffer data: raw data from select APDU response
+
+                console.log(`${reader.reader.name}  card detected`, card);
+
+            });
+
+            reader.on('card.off', card => {
+                console.log(`${reader.reader.name}  card removed`, card);
+            });
+
+            reader.on('error', err => {
+                console.log(`${reader.reader.name}  an error occurred`, err);
+            });
+
+            reader.on('end', () => {
+                console.log(`${reader.reader.name}  device removed`);
+            });
+
+        });
+
 
         for (let i = 0; i < this.amountOfCourts; i++) {
             this.addCourt();
@@ -362,14 +413,26 @@
 
             // sets back the players if < 4 on court
             this.courts.forEach((court) => {
-                if (court.players.length < 4) {
-                    court.players.forEach( (player) => {
-                        player.oncourt = false;
-                        let behindQueue = this.participants.splice( this.participants.findIndex(par => par.speelNummer === player.speelNummer), 1);                 
-                        this.participants.push(behindQueue[0]);
-                    });
-                    court.players = [];
+                if (court.isDouble) {
+                    if (court.players.length < 4) {
+                        court.players.forEach( (player) => {
+                            player.oncourt = false;
+                            let behindQueue = this.participants.splice( this.participants.findIndex(par => par.speelNummer === player.speelNummer), 1);                 
+                            this.participants.push(behindQueue[0]);
+                        });
+                        court.players = [];
+                    } 
+                } else {
+                     if (court.players.length < 2) {
+                        court.players.forEach( (player) => {
+                            player.oncourt = false;
+                            let behindQueue = this.participants.splice( this.participants.findIndex(par => par.speelNummer === player.speelNummer), 1);                 
+                            this.participants.push(behindQueue[0]);
+                        });
+                        court.players = [];
+                    } 
                 }
+                
             });
         },
 
